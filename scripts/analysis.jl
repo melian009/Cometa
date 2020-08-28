4f5d531fa52a5db905fde01a6ee676f939a64b57
Ali R. Vahdati <arv.kavir@gmail.com>
2020-08-28 15:14:45 +0200

    Shannon diversity index heatmaps of simulations

--- scripts/analysis.jl
+++ scripts/analysis.jl
@@ -2,9 +2,16 @@ using DrWatson
 @quickactivate
 using CSV
 using DataFrames
+using Makie
 include(srcdir("julia", "functions.jl"))
 
 magicStrong = datadir("sims","MagicStrong")
 magicWeak = datadir("sims","MagicWeak")
 modularStrong = datadir("sims","ModularStrong")
 modularWeak = datadir("sims","ModularWeak")
+
+## Shannon diversity index
+shannondiv = shannonIndex(joinpath(magicStrong, "data_10.csv"))
+p = heatmap(1:10, 1:101, shannondiv')
+axes = p[Axis];
+axes.names.axisnames = ("Site", "Generation")
--- src/julia/functions.jl
+++ src/julia/functions.jl
@@ -1,19 +1,25 @@
 # These functions are used in analysis.jl
 
-function read_sims(csvfile)
-  df = CSV.read(csvfile, DataFrame)
-end
+"""
+    shannonIndex(csvfile; nspecies=10)
 
-function shannonIndex(model)
-  output = zeros(nv(model))
-  for node in 1:nv(model)
-    species_freq = zeros(model.properties[:nspecies])
-    for ag in get_node_contents(node, model)
-      species_freq[model.agents[ag].species] += 1
-    end
-    species_freq = species_freq ./ sum(species_freq)
-    shannon = - sum(log2.(species_freq) .* species_freq)
-    output[node] = shannon
+Calculates Shannon diversity index per site.
+The function input is a simulation result file in CSV format.
+
+# keyword arguments
+* nspecies=10: maximum number of species
+"""
+function shannonIndex(csvfile; nspecies=10)
+  df = CSV.read(csvfile, DataFrame)
+  nrows, ncols = size(df)
+  nsites = Int((ncols-1) / nspecies)
+  generations = zeros(nrows, nsites)
+  for generation in 1:nrows
+    m = reshape(Array(df[generation, 2:end]), nspecies, nsites)
+    totalsum = sum(m, dims=1)
+    species_freq = m ./ totalsum
+    shannon = - sum(log2.(species_freq) .* species_freq, dims=1)
+    generations[generation, :] = shannon
   end
-  return output
+  return generations
 end
