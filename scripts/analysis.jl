92be41aa25c50ff056f2139a9da29b8398dec68d
Ali R. Vahdati <arv.kavir@gmail.com>
2020-08-28 14:35:33 +0200

    Update simulation jl files

--- scripts/analysis.jl
+++ scripts/analysis.jl
@@ -2,9 +2,9 @@ using DrWatson
 @quickactivate
 using CSV
 using DataFrames
+include(srcdir("julia", "functions.jl"))
 
 magicStrong = datadir("sims","MagicStrong")
 magicWeak = datadir("sims","MagicWeak")
 modularStrong = datadir("sims","ModularStrong")
 modularWeak = datadir("sims","ModularWeak")
-
--- src/julia/functions.jl
+++ src/julia/functions.jl
@@ -3,3 +3,17 @@
 function read_sims(csvfile)
   df = CSV.read(csvfile, DataFrame)
 end
+
+function shannonIndex(model)
+  output = zeros(nv(model))
+  for node in 1:nv(model)
+    species_freq = zeros(model.properties[:nspecies])
+    for ag in get_node_contents(node, model)
+      species_freq[model.agents[ag].species] += 1
+    end
+    species_freq = species_freq ./ sum(species_freq)
+    shannon = - sum(log2.(species_freq) .* species_freq)
+    output[node] = shannon
+  end
+  return output
+end
