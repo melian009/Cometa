80214e778e288d03379d2c732f54e0c9f5e91d67
Ali R. Vahdati <arv.kavir@gmail.com>
2022-04-30 14:44:55 +0200

    update random model

--- scripts/random/random.jl
+++ scripts/random/random.jl
@@ -1,9 +1,9 @@
 using EvoDynamics
-using Agents
-using Distributions
-using Random
-using LinearAlgebra
-
+# using Agents
+# using Distributions
+# using Random
+# using LinearAlgebra
 
 param_file = "params.yml"
 adata, mdata, models = runmodel(param_file)
+# it works if I run it twice!
