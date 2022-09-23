cf0a4b75c584ab5e29abfbc72bb0c382f91f2cab
Ali R. Vahdati <arv.kavir@gmail.com>
2022-09-23 13:06:43 +0200

    Minor param update

--- scripts/SimulationsV2/random/random.jl
+++ scripts/SimulationsV2/random/random.jl
@@ -1,9 +1,16 @@
-using EvoDynamics
+# using Pkg
+# Pkg.activate(".")
+using Distributed
+addprocs(5)
+@everywhere using EvoDynamics
+using JLD2
+using FileIO
 # using Agents
 # using Distributions
 # using Random
 # using LinearAlgebra
 
 param_file = "params.jl"
-adata, mdata, models = runmodel(param_file)
-# it works if I run it twice!
+adata, mdata, models = runmodel(param_file, replicates=6, parallel=true)
+
+save("output.jld2", "results", mdata)
