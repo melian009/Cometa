13edaecf4ae1b6cdcac9f1ac75778569965eae64
Ali R. Vahdati <arv.kavir@gmail.com>
2022-12-20 13:34:10 +0100

    Create modular and correlated sims
    With varying values for migration threshold and biotic variance

---
+++ scripts/SimulationsV2/modular_vs_correlated/README.md
@@ -0,0 +1,22 @@
+* We want to explore a range of simulations, each one having a different migration rate and biotic selection coefficient (`biotic_variance`).
+* 10 Replicates per combination.
+* There is not mutation or evolving trait architecture.
+* There are two set of simulations: one is **modular** and the other is **correlated**. 
+
+## Modular
+
+This is a fully modular pleiotropy matrix:
+
+ 1  1  1  1  1  1  1  1  1  1  0  0  0  0  0  0  0  0  0  0  0
+ 0  0  0  0  0  0  0  0  0  0  1  1  1  1  1  1  1  1  1  1  0
+ 0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  1  # this is migration
+
+This is a fully correlated pleiotropy matrix:
+
+ 1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1
+ 1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1
+ 1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1
+
+## Food resources matrix
+
+* We keep a fixed food_resources matrix and run the simulations. In another set, we randomize the `food_resources` matrix. To randomize, we can keep the number of non-zeros and predator, preys, but random combination. The `interactions` matrix will always follow the `food_resources` matrix, having a 1 where ever there is a non-zero value in the `food_resources`.
---
+++ scripts/SimulationsV2/modular_vs_correlated/main.jl
@@ -0,0 +1,59 @@
+using Distributed
+using Pkg
+Pkg.activate(".")
+addprocs(6)
+@everywhere using EvoDynamics
+using JLD2
+using FileIO
+
+
+pf = "parameters_frame.jl"
+paramdir = "parameter_files/"
+results_dir = "sim_outputs/"
+
+migration_thresholds = [0.0, 0.1, 1.0, 2.0, 5.0, 10.0]
+biotic_variances = [0.0, 0.1, 1.0, 2.0, 5.0, 10.0]
+
+function create_single_parameter_file(pf, migration_rate, biotic_coeff, paramdir=paramdir)
+  if !isdir(paramdir)
+    mkdir(paramdir)
+  end
+  mkdir(paramdir)
+  outfile = joinpath(paramdir, "params_migration_rate=$(migration_rate)_biotic_coeff=$(biotic_coeff)_fixed_interaction_mat.jl")
+  input = readlines(pf)
+  # Update migration threshold
+  lines = findall(x -> startswith(strip(x), ":migration_threshold"), input)
+  input[lines] .= ":migration_threshold => $(migration_rate),"
+
+  # Update biotic variance
+  lines = findall(x -> startswith(strip(x), ":biotic_variance"), input)
+  input[lines] .= ":biotic_variance => $(biotic_coeff),"
+
+  open(outfile, "w") do ff
+    for line in input
+      println(ff, line)
+    end
+  end
+  return outfile
+end
+
+function create_parameter_combinations(pf, migration_rates, biotic_coeffs)
+  for mrate in migration_rates
+    for biocoef in biotic_coeffs
+      create_single_parameter_file(pf, mrate, biocoef)
+    end
+  end
+end
+
+create_parameter_combinations(pf, migration_thresholds, biotic_variances)
+
+all_parameter_files = readdir(paramdir)
+
+for f in all_parameter_files
+  param_file = joinpath(paramdir, f)
+  adata, mdata, models = runmodel(param_file, replicates=7, parallel=true, showprogress=true)
+  if !isdir(results_dir)
+    mkdir(results_dir)
+  end
+  save(joinpath(results_dir, "$(f[1:end-3]).jld2"), "results", mdata)
+end
