2819ff9187081e0940043b07cb57d245f2b618c7
Ali R. Vahdati <3798865+kavir1698@users.noreply.github.com>
2024-08-30 18:14:25 +0200

    Refactor simulation scripts to check for existing output files before running

--- scripts/SimulationsV2/modular_vs_correlated2/run_sims_mod.sh
+++ scripts/SimulationsV2/modular_vs_correlated2/run_sims_mod.sh
@@ -15,7 +15,13 @@ for ((i=0; i<${#files[@]}; i+=10)); do
     for ((j=i; j<i+10 && j<${#files[@]}; j++)); do
         f=${files[$j]}
         filename=$(basename "$f")
-        julia run_sims_bash_mod.jl "$filename" "$paramdir" "$results_dir" "$nreplicates" &
+        output_file="${results_dir}/${filename%.jl}.jld2"
+        
+        if [ ! -f "$output_file" ]; then
+            julia run_sims_bash_mod.jl "$filename" "$paramdir" "$results_dir" "$nreplicates" &
+        else
+            echo "Output file $output_file already exists. Skipping simulation for $filename."
+        fi
     done
     wait # Wait for all background processes to finish before continuing
 done
